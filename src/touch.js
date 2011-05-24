02e526365695228b26d8d20b9235b255d8538b6b
Ross Hale <dev@lottay.com>
2011-05-24 14:40:33 -0700

    rh - removing double-click from touch.js cuz we're not using it and it's slowing us down!

--- src/touch.js
+++ src/touch.js
@@ -16,31 +16,21 @@
 
   $(document).ready(function(){
     $(document.body).bind('touchstart', function(e){
-      var now = Date.now(), delta = now - (touch.last || now);
       touch.target = parentIfText(e.touches[0].target);
-      touchTimeout && clearTimeout(touchTimeout);
       touch.x1 = e.touches[0].pageX;
       touch.y1 = e.touches[0].pageY;
-      if (delta > 0 && delta <= 250) touch.isDoubleTap = true;
-      touch.last = now;
     }).bind('touchmove', function(e){
       touch.x2 = e.touches[0].pageX;
       touch.y2 = e.touches[0].pageY;
     }).bind('touchend', function(e){
-      if (touch.isDoubleTap) {
-        $(touch.target).trigger('doubleTap');
-        touch = {};
-      } else if (touch.x2 > 0 || touch.y2 > 0) {
+      if (touch.x2 > 0 || touch.y2 > 0) {
         (Math.abs(touch.x1 - touch.x2) > 30 || Math.abs(touch.y1 - touch.y2) > 30)  &&
           $(touch.target).trigger('swipe') &&
           $(touch.target).trigger('swipe' + (swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2)));
         touch.x1 = touch.x2 = touch.y1 = touch.y2 = touch.last = 0;
-      } else if ('last' in touch) {
-        touchTimeout = setTimeout(function(){
-          touchTimeout = null;
-          $(touch.target).trigger('tap')
-          touch = {};
-        }, 250);
+      } else {
+        $(touch.target).trigger('tap');
+        touch = {};
       }
     }).bind('touchcancel', function(){ touch = {} });
   });
